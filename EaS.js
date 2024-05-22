80b8c9609dfa6dcc0b0032fb0d8aff6faedea0c6
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2024-05-23 01:57:41 +0700

    Tweak around a bit and managed to make the grid looks more correct.

--- EaS.js
+++ EaS.js
@@ -14,8 +14,9 @@ let gridSizeButton = document.querySelector(".grid-size");
 gridSizeButton.addEventListener("click", function() {
     let userInput = Number(prompt(("Enter a grid size up to 100")));
     if (userInput > 100) {
+        alert("Only numbers between 2 and 100!");
         userInput = 16;
-    } else if (userInput <= 0) {
+    } else if (userInput <= 1) {
         userInput = 16;
     } else if (isNaN(userInput)) {
         alert("That's not a number!");
@@ -29,16 +30,16 @@ gridSizeButton.addEventListener("click", function() {
 
     let gridSize = userInput * userInput;
     if (gridSize <= 10000) {
-            for (i = 0; i < gridSize; i++) {
-                let i = document.createElement("div");
-                i.classList.add("square");
-                i.style.height = `${7680 / gridSize}px`;
-                i.style.width = `${7680 / gridSize}px`;
-                container.appendChild(i);
-                i.addEventListener("mouseover", function() {
-                    i.style.backgroundColor = "black";
-                })
-            }
+        for (i = 0; i < gridSize; i++) {
+          let i = document.createElement("div");
+          i.classList.add("square");
+          i.style.height = `${400 / userInput}px`;
+          i.style.width = `${400 / userInput}px`;
+          container.appendChild(i);
+          i.addEventListener("mouseover", function() {
+            i.style.backgroundColor = "black";
+          })
         }
+    }
 })
 
