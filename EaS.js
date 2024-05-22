66b07651899c0b140fce59a0651e9bfb1c9a0f48
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2024-05-22 16:13:53 +0700

    Square now turn black when mouseover.

--- EaS.js
+++ EaS.js
@@ -3,7 +3,10 @@ let container = document.querySelector(".container");
 for (i = 0; i < 256; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
-    container.appendChild(square); 
+    container.appendChild(square);
+    square.addEventListener("mouseover", function() {
+        square.style.backgroundColor = "black";
+    }) 
 }
 
 
